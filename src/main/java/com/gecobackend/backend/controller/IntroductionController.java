f608384078097915b571644231cf3fb50b6067e7
Hitesh Ahuja <hiteshahuja@Hiteshs-MacBook-Air.local>
2022-03-24 17:31:36 +0530

    CORS change

--- src/main/java/com/gecobackend/backend/controller/IntroductionController.java
+++ src/main/java/com/gecobackend/backend/controller/IntroductionController.java
@@ -1,18 +1,19 @@
 package com.gecobackend.backend.controller;
 
-import javax.validation.constraints.Null;
 
 import com.gecobackend.backend.model.Introduction;
 import com.gecobackend.backend.service.IntroductionService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin
 @RestController
 public class IntroductionController {
 
